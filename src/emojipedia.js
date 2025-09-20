805287a2656f01159f43bae75849d5c744a5aa10
Thejanu Shanthusha <thejanushanthusha@gmail.com>
2025-09-20 10:57:05 +0530

    Add Emojipedia React app

---
+++ src/emojipedia.js
@@ -0,0 +1,32 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😅",
+    name: "Smiling Face With Open Mouth & Cold Sweat",
+    meaning:
+      "A yellow face with a big open (grinning) mouth, uplifted eyebrows, and a single bead of sweat, usually on the left side of its forehead. Conveys nervousness and relief, or a close call.",
+  },
+];
+
+export default emojipedia;
